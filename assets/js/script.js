c23b162f4a4a33272deb1eafb0d6feaf49a17b88
km4891 <kmorarity@gmail.com>
2020-09-13 23:38:59 -0700

    js update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,36 @@
-var getCurrentWeather = function() {
-    var openWeatherApi = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=7ad577f94ffbc59a26490910c626df8b";
-    fetch(openWeatherApi)
+var cityFormEl = document.querySelector("#city-form");
+var cityInputEl = document.querySelector("#city-input");
+var citiesContainerEl = document.querySelector("#cities-container");
+var citiesSearchTerm = document.querySelector("#cities-search-term");
 
+
+var getUserRepos = function(cityName) {
+    // format the github api url
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=7ad577f94ffbc59a26490910c626df8b";
+  
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+      response.json().then(function(data) {
+        displayCurrentWeather(data, cityName)
+      });
+    });
+  };
+
+  var formSubmitHandler = function(event) {
+    event.preventDefault();
+    var cityName = cityInputEl.value.trim();
+
+if (cityName) {
+  getUserRepos(cityName);
+  cityInputEl.value = "";
+} else {
+  alert("Please enter a valid city");
 }
+  };
 
-getCurrentWeather(memphis);
+
+
+    
+
+
+  cityFormEl.addEventListener("submit", formSubmitHandler);
